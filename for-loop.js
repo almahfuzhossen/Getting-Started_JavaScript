71a6c57d1cd2bf244b30aa07b9de3c1f7c7a4224
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-07 03:17:00 +0600

    for loop practice

--- for-loop.js
+++ for-loop.js
@@ -7,8 +7,24 @@
 // }
 
 
+// for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('roast den, please!!')
+//     console.log(roastGiven);
+// }
 
-for(var roastGiven = 0; roastGiven < 7; roastGiven++){
-    console.log('roast den, please!!')
-    console.log(roastGiven);
+// for (var i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+//odd
+// i+=2;
+//i = i+2
+
+// for(var i= 1; i<20; i += 2){
+// console.log(i);
+// }
+
+//even
+for(var i=0; i<20; i+=2){
+    console.log(i);
 }
