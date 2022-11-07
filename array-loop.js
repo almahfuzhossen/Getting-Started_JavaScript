6ddd2841260ddeef27d2d025a3ab2a3489bbe439
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-08 02:08:43 +0600

    array loop

---
+++ array-loop.js
@@ -0,0 +1,21 @@
+
+//basic while loop
+
+// var i = 0;
+// while (i<=7){
+//     console.log(i);
+//     i++;
+// }
+
+//basic for loop
+
+// for( var i=0; i<=10; i++){
+//     console.log(i);
+// }
+
+
+//  var numbers=[33, 22, 21, 45, 47, 12];
+//  for (var i=0; i < numbers.length; i++){
+//      var element = numbers[i];
+//       console.log(element);
+//  }
