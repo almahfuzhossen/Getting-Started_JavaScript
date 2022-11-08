2b7a559417acb0e9a0a673554debdd7eb5c292c5
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-09 01:19:39 +0600

    parametar added

---
+++ parametar.js
@@ -0,0 +1,11 @@
+function bringSingara(taka) {
+    console.log('singra r jonne tk dise', taka);
+    console.log('mama singara den');
+    var singaraPrice = 10;
+    var singaraQuantity = taka / singaraPrice;
+    return singaraQuantity;
+    
+}
+
+ var money = 250;
+ var singara = bringSingara(money); 
