e7affb230459d2f5dba2caae45d7bcc87bbccaf2
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-08 13:11:29 +0600

    odd number with while loop

--- while-loop-example.js
+++ while-loop-example.js
@@ -3,3 +3,12 @@
 //     console.log(number);
 //     number++;
 // }
+
+
+// odd number
+
+// var number = 1;
+// while (number <= 20){
+//     console.log(number);
+//     number = number + 2;
+// }
