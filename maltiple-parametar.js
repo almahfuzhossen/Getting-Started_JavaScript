30fbda481c8ff988a262d66f655968d36241bf57
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-09 02:29:41 +0600

    added more example related multiple parametar

--- maltiple-parametar.js
+++ maltiple-parametar.js
@@ -7,13 +7,35 @@
 // console.log('total multiplication', total);
 
 
-function addTwoNumbers(number1, number2) {
-    console.log(number1, number2);
-    var total = number1 + number2;
-    return total;
-    }
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1, number2);
+//     var total = number1 + number2;
+//     return total;
+//     }
     
-    var FirstNumber = 35;
-    var SecondNumber = 22;
-     var result = addTwoNumbers(FirstNumber, SecondNumber);
-     console.log('result value:', result);
+//     var FirstNumber = 35;
+//     var SecondNumber = 22;
+//      var result = addTwoNumbers(FirstNumber, SecondNumber);
+//      console.log('result value:', result);
+
+    //biyog korbo..
+
+    // function evenTwoNumbers(number1 , number2) {
+    //     var result = number1 - number2;
+    //     return result;
+    // }
+    // var FirstNumber = 22;
+    // var SecondNumber = 7;
+    // var result = evenTwoNumbers(FirstNumber, SecondNumber);
+    // console.log('result value:', result);
+
+    //ami ekhon javaScript diye vag kori..
+
+    function divideTwoNumbers(num1, num2) {
+        var result = num1 / num2;
+        return result;
+    }
+    var FirstNumber = 50;
+    var SecondNumber = 10;
+    var result = divideTwoNumbers(FirstNumber, SecondNumber);
+    console.log('result value:', result);
