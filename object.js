0af38228d7d8477a8e94857469ac2b71005201f2
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-09 03:01:57 +0600

    multiple objs with multiple properties

---
+++ object.js
@@ -0,0 +1,27 @@
+var studen1 {
+    id : 119,
+    name : 'mahfuz',
+    marks : 91,
+    phone : 0188149
+    
+};
+var phone {
+Color : 'black',
+Ram : 4,
+Rom : 7,
+Price : 19000,
+screenSize : 5,
+storage : '16gb'
+
+
+};
+
+var book {
+    name : 'khondokar gk',
+    totalPage: 870,
+    height : '2m',
+    width : '1m',
+    textcolor : 'black',
+    price : 320,
+    publisher : 'khandokar prokashoni'
+};
