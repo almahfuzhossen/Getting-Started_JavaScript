a707ea7d72db5a6ff71fa02290644a6e603c9389
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-09 02:11:27 +0600

    maltiple parametar added

---
+++ maltiple-parametar.js
@@ -0,0 +1,19 @@
+
+//  function multiplyTwoNumbers(num1, num2) {
+//     var result = num1 * num2;
+//     return result;
+// }
+// var total = multiplyTwoNumbers(5, 100);
+// console.log('total multiplication', total);
+
+
+function addTwoNumbers(number1, number2) {
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+    }
+    
+    var FirstNumber = 35;
+    var SecondNumber = 22;
+     var result = addTwoNumbers(FirstNumber, SecondNumber);
+     console.log('result value:', result);
