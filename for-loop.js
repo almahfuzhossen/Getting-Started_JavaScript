fdf4bb0dbb231f8daf4bf229ad8592c28503f091
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-08 14:34:32 +0600

    for loop added

---
+++ for-loop.js
@@ -0,0 +1,14 @@
+// var roastGiven = 0;
+
+// while (roastGiven < 7) {
+//     console.log('roast den, please!!');
+//     roastGiven++;
+//     console.log(roastGiven);
+// }
+
+
+
+for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('roast den, please!!')
+    console.log(roastGiven);
+}
