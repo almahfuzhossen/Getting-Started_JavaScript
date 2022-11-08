8d50eb0a3f34debe9a6d9c8bbccc4a361e6e409c
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-09 04:03:48 +0600

    obj-properties

--- obj-properties.js
+++ obj-properties.js
@@ -1,1 +1,28 @@
-var computer
+var computer ={
+    price : 30000,
+    processor : 'intel i5',
+    color : 'silver',
+    storage : '140gb'
+}
+
+//set a property value
+computer.price= 22000;
+//console.log(computer);
+
+//console.log(computer);
+//console.log(computer.processor);
+var computerPrice = computer.processor;
+//console.log(computerPrice);
+
+//different ways to set of an object property
+
+//computer.price= 22000;-- 1
+//console.log(computer);-- 1
+
+
+// computer["price"]=23000; -- 2
+// console.log(computer);   --2
+
+var priceProperty = "price";
+computer[priceProperty] = 23400;
+console.log(computer);
