b01910ac52ff51e9eff6384a6e6c9a0f24ba90f0
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-09 12:31:59 +0600

    function declaration

---
+++ function.js
@@ -0,0 +1,13 @@
+//function declaration
+
+function startFan() {
+    console.log('walk towards the switch')
+    console.log('press the button');
+}
+//call the function
+startFan();
+
+console.log('need a tea');
+console.log('read the book');
+console.log('need a pen');
+startFan();
