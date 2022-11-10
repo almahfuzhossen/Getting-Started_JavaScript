807b0e93d03a957e825c9d86951a0f4716a0c485
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-10 11:25:26 +0600

    let-const added

---
+++ let-const.js
@@ -0,0 +1,16 @@
+ 
+ //value of variable could change..
+ //because of we can use 'let'
+ 
+ let price = 27;
+ price = 22;
+ price = 88;
+ price = 33;
+
+
+ //value of variable could not change.
+ //because of we are using 'const'
+
+ const myName = 'mr. marsh';
+ console.log(myName);
+ myName = 22;
