8ae4b9a86a6d8b1724a9b2784cbca26a73cd9e3d
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-12 10:44:09 +0600

    git commit

--- math.js/mathrandom.js
+++
@@ -1,9 +0,0 @@
-
-// const myNumber = 2.35346;
-// const output = Math.ceil(myNumber);
-// console.log(output);
-
-
-// const myNumber = 5.88769;
-// const output = Math.floor(myNumber);
-// console.log(output);
---
+++ mathrandom.js
@@ -0,0 +1,21 @@
+
+
+
+// const myNumber = 5.88769;
+// const output = Math.floor(myNumber);
+// console.log(output);
+
+
+// const myNumber = 5.88769;
+// const output = Math.round(myNumber);
+// console.log(output);
+
+
+// const myNumber = 5.88;
+// const output = Math.random();
+// console.log(output);
+
+
+const myNumber = 5.88;
+const output = Math.random()*5;
+console.log(output);
